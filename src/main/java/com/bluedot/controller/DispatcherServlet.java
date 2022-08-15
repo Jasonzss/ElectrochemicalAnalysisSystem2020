5cb68a3ce3a7c3a37bfc0fe10531fd2286a0a5f8
Jason <18479385003@163.com>
2022-08-15 11:54:40 +0800

    init

---
+++ src/main/java/com/bluedot/controller/DispatcherServlet.java
@@ -0,0 +1,6 @@
+package com.bluedot.controller;/**
+ * @Author Jason
+ * @CreationDate 2022/08/15 - 11:54
+ * @Description ：
+ */public class DispatcherServlet {
+}
---
+++ src/main/java/com/bluedot/mapper/BaseMapper.java
@@ -0,0 +1,6 @@
+package com.bluedot.mapper;/**
+ * @Author Jason
+ * @CreationDate 2022/08/15 - 11:53
+ * @Description ：
+ */public class BaseMapper {
+}
