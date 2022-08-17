b8ad56f62f7a0fd6633b3c30471a17635cdf707b
zoulinjiang <zoulinjiang@itcast.cn>
2022-08-17 09:42:00 +0800

    feat(StringUtil，ReflectUtil):新增mapper工具类

--- src/main/java/com/bluedot/utils/ReflectUtil.java
+++ src/main/java/com/bluedot/utils/ReflectUtil.java
@@ -1,10 +1,38 @@
 package com.bluedot.utils;
 
-/**
- * @Author Jason
- * @CreationDate 2022/08/15 - 11:52
- * @Description ：
- */
-public class ReflectUtil {
 
+
+import java.lang.reflect.Method;
+
+
+public class ReflectUtil {
+    public static Object invokeGet(Object object, String fieldName) {
+        Class<?> clazz = object.getClass();
+        Method method;
+        Object res;
+        try {
+            method = clazz.getDeclaredMethod("get" + StringUtil.firstCharToUpperCase(fieldName));
+            res = method.invoke(object);
+        } catch (Exception e) {
+            throw new RuntimeException("[" + Thread.currentThread().getName() + "]" +
+                    "com.xxbb.smybatis.utils.ReflectUtils" + "--->" +
+                    e.getMessage());
+        }
+        return res;
+    }
+
+    public static void invokeSet(Object obj, String columnName, Object value) {
+        Class<?> clazz = obj.getClass();
+        Method method;
+        try {
+            method = clazz.getDeclaredMethod("set" + StringUtil.columnNameToMethodName(columnName), value.getClass());
+            method.setAccessible(true);
+            method.invoke(obj, value);
+        } catch (Exception e) {
+            throw new RuntimeException("[" + Thread.currentThread().getName() + "]" +
+                    "com.xxbb.smybatis.utils.ReflectUtils" + "--->" + "value=" +
+                    value.getClass());
+        }
+
+    }
 }
