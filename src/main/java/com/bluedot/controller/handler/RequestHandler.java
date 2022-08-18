1585ebef05d1f4de64ccdb7a5ba62880a0d7f40f
songjinjin <6020202300@qq.com>
2022-08-18 15:59:59 +0800

    refactor: 完善请求处理器功能

--- src/main/java/com/bluedot/controller/handler/RequestHandler.java
+++ src/main/java/com/bluedot/controller/handler/RequestHandler.java
@@ -1,5 +1,21 @@
 package com.bluedot.controller.handler;
 
-public interface RequestHandler {
+import com.bluedot.pojo.Dto.Data;
+import com.bluedot.pojo.vo.CommonResult;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * @Author SDJin
+ * @CreationDate 2022/08/16 - 11:54
+ * @Description ：
+ */
+public interface RequestHandler {
+    /**
+     * 处理请求
+     * @param request 请求request对象
+     * @param response 响应response对象
+     * @return 请求处理结果CommonResult对象
+     */
+    CommonResult handlerRequest(HttpServletRequest request, HttpServletResponse response);
 }
