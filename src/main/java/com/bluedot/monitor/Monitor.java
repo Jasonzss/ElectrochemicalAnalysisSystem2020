99f5c901cf438b8ebda8b0d1fde97c9db939feff
songjinjin <6020202300@qq.com>
2022-08-18 15:58:28 +0800

    docs: 新增注解

--- src/main/java/com/bluedot/monitor/Monitor.java
+++ src/main/java/com/bluedot/monitor/Monitor.java
@@ -1,7 +1,13 @@
 package com.bluedot.monitor;
-
-
+/**
+ * @Author SDJin
+ * @CreationDate 2022/08/16 - 11:54
+ * @Description ：
+ */
 public abstract class Monitor<T> implements  Runnable{
+     /**
+      * 监听的队列
+      */
      protected T queue;
 
 }
