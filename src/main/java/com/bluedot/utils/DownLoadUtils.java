36ace16da0303e0426e1a6fa6d468a530ec5fcb5
songjinjin <6020202300@qq.com>
2022-08-23 17:21:00 +0800

    feat: 新增文件下载工具类

---
+++ src/main/java/com/bluedot/utils/DownLoadUtils.java
@@ -0,0 +1,25 @@
+package com.bluedot.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Base64;
+
+
+public class DownLoadUtils {
+
+    public static String getFileName(String agent, String filename) throws UnsupportedEncodingException {
+        if (agent.contains("MSIE")) {
+            // IE浏览器
+            filename = URLEncoder.encode(filename, "utf-8");
+            filename = filename.replace("+", " ");
+        } else if (agent.contains("Firefox")) {
+            // 火狐浏览器
+            Base64.Encoder encoder = Base64.getEncoder();
+            filename = "=?utf-8?B?" +encoder.encodeToString(filename.getBytes("utf-8")) + "?=";
+        } else {
+            // 其它浏览器
+            filename = URLEncoder.encode(filename, "utf-8");
+        }
+        return filename;
+    }
+}
