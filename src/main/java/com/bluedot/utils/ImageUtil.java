eb33d987d64c05a64d253f1dd4ae0cd53dfef87e
Jason <18479385003@163.com>
2022-08-23 17:20:07 +0800

    feat(ImageUtil): 新增图片工具类

--- src/main/java/com/bluedot/utils/ImageUtil.java
+++ src/main/java/com/bluedot/utils/ImageUtil.java
@@ -3,7 +3,9 @@ package com.bluedot.utils;
 import org.apache.commons.fileupload.FileItem;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import java.util.Random;
 
 
@@ -12,39 +14,198 @@ import java.util.Random;
  * @CreationDate 2022/08/16 - 21:47
  * @Description ：
  */
+
 public class ImageUtil {
-    public static Byte[][] imgToBinary(FileItem userImg) {
-        return null;
-    }
+    /**
+     * 使用到Algerian字体，系统里没有的话需要安装字体，字体只显示大写，去掉了1,0,I,O几个容易混淆的字符
+     */
+    public static final String VERIFY_CODES = "23456789ABCDEFGHJKLMNPQRSTUVWXYZ";
+    private static Random random = new Random();
 
-    public static BufferedImage createAuthImg(String code){
-        //在内存中创建一个图片
-        BufferedImage image = new BufferedImage(220,40,BufferedImage.TYPE_INT_RGB);
+    public static int verifySize;
+    public static final int WIDTH = 220;
+    public static final int HEIGHT = 50;
 
-        //得到图片
-        //画笔
-        Graphics2D g = (Graphics2D) image.getGraphics();
-        //设置图片的背景颜色
-        g.setColor(Color.WHITE);
-        g.fillRect(0,0,220,40);
-        //给图片写数据
-        g.setColor(Color.GRAY);
+    public static BufferedImage createAuthImage(String code){
+        verifySize = code.length();
 
-        //设置随机字体
-        String[] fontName = new String[3];
-        fontName[0] = "DialogInput";
-        fontName[1] = "DialogInput";
-        fontName[2] = "SansSerif";
+        BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        Random rand = new Random();
+        Graphics2D g2 = image.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Color[] colors = new Color[5];
+        Color[] colorSpaces = new Color[] { Color.WHITE, Color.CYAN, Color.GRAY, Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE, Color.PINK,
+                Color.YELLOW };
+        float[] fractions = new float[colors.length];
+        for (int i = 0; i < colors.length; i++) {
+            colors[i] = colorSpaces[rand.nextInt(colorSpaces.length)];
+            fractions[i] = rand.nextFloat();
+        }
+        Arrays.sort(fractions);
 
-        for (int i = 0; i < code.length(); i++) {
-            //随机数字大小
-            int size = new Random().nextInt(10) + 22;
+        // 设置边框色
+        g2.setColor(Color.GRAY);
+        g2.fillRect(0, 0, WIDTH, HEIGHT);
 
-            //挨个画出字符
-            g.setFont(new Font(fontName[size%3],Font.BOLD,size));
-            g.drawString(String.valueOf(code.charAt(i)),25*i,30);
+        Color c = getRandColor(200, 250);
+        // 设置背景色
+        g2.setColor(c);
+        g2.fillRect(0, 2, WIDTH, HEIGHT - 4);
+        // 绘制干扰线
+        Random random = new Random();
+        // 设置线条的颜色
+        g2.setColor(getRandColor(160, 200));
+        for (int i = 0; i < 20; i++) {
+            int x = random.nextInt(WIDTH - 1);
+            int y = random.nextInt(HEIGHT - 1);
+            int xl = random.nextInt(6) + 1;
+            int yl = random.nextInt(12) + 1;
+            g2.drawLine(x, y, x + xl + 40, y + yl + 20);
+        }
+
+        // 添加噪点
+        // 噪声率
+        float yawpRate = 0.05f;
+        int area = (int) (yawpRate * WIDTH * HEIGHT);
+        for (int i = 0; i < area; i++) {
+            int x = random.nextInt(WIDTH);
+            int y = random.nextInt(HEIGHT);
+            int rgb = getRandomIntColor();
+            image.setRGB(x, y, rgb);
+        }
+
+        // 使图片扭曲
+        shear(g2, WIDTH, HEIGHT, c);
+
+        g2.setColor(getRandColor(100, 160));
+        int fontSize = HEIGHT - 4;
+        Font font = new Font("Algerian", Font.ITALIC, fontSize);
+        g2.setFont(font);
+        char[] chars = code.toCharArray();
+        for (int i = 0; i < verifySize; i++) {
+            AffineTransform affine = new AffineTransform();
+            affine.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1), (WIDTH / verifySize) * i + fontSize / 2, HEIGHT / 2);
+            g2.setTransform(affine);
+            g2.drawChars(chars, i, 1, ((WIDTH - 10) / verifySize) * i + 5, HEIGHT / 2 + fontSize / 2 - 10);
         }
 
         return image;
+    }
+
+
+
+
+    /**
+     * 使用系统默认字符源生成验证码
+     * @param verifySize    验证码长度
+     * @return 返回指定长度的验证码字符串
+     */
+    public static String generateVerifyCode(int verifySize) {
+        return generateVerifyCode(verifySize, VERIFY_CODES);
+    }
+
+    /**
+     * 使用指定源生成验证码
+     * @param verifySize    验证码长度
+     * @param sources   验证码字符源
+     * @return 返回指定长度和字符的验证码字符串
+     */
+    public static String generateVerifyCode(int verifySize, String sources) {
+        if (sources == null || sources.length() == 0) {
+            sources = VERIFY_CODES;
+        }
+        int codesLen = sources.length();
+        Random rand = new Random(System.currentTimeMillis());
+        StringBuilder verifyCode = new StringBuilder(verifySize);
+        for (int i = 0; i < verifySize; i++) {
+            verifyCode.append(sources.charAt(rand.nextInt(codesLen - 1)));
+        }
+        return verifyCode.toString();
+    }
+
+    /**
+     * 获取随机颜色
+     */
+    private static Color getRandColor(int fc, int bc) {
+        if (fc > 255) {
+            fc = 255;
+        }
+        if (bc > 255) {
+            bc = 255;
+        }
+        int r = fc + random.nextInt(bc - fc);
+        int g = fc + random.nextInt(bc - fc);
+        int b = fc + random.nextInt(bc - fc);
+        return new Color(r, g, b);
+    }
+
+    private static int getRandomIntColor() {
+        int[] rgb = getRandomRgb();
+        int color = 0;
+        for (int c : rgb) {
+            color = color << 8;
+            color = color | c;
+        }
+        return color;
+    }
+
+    private static int[] getRandomRgb() {
+        int[] rgb = new int[3];
+        for (int i = 0; i < 3; i++) {
+            rgb[i] = random.nextInt(255);
+        }
+        return rgb;
+    }
+
+    private static void shear(Graphics g, int w1, int h1, Color color) {
+        shearX(g, w1, h1, color);
+        shearY(g, w1, h1, color);
+    }
+
+    private static void shearX(Graphics g, int w1, int h1, Color color) {
+
+        int period = random.nextInt(2);
+
+        boolean borderGap = true;
+        int frames = 1;
+        int phase = random.nextInt(2);
+
+        for (int i = 0; i < h1; i++) {
+            double d = Math.sin((double) i / (double) period + (6.2831853071795862D * phase) / frames);
+            g.copyArea(0, i, w1, 1, (int) d, 0);
+            if (borderGap) {
+                g.setColor(color);
+                g.drawLine((int) d, i, 0, i);
+                g.drawLine((int) d + w1, i, w1, i);
+            }
+        }
+
+    }
+
+    private static void shearY(Graphics g, int w1, int h1, Color color) {
+        // 50
+        int period = random.nextInt(40) + 10;
+
+        boolean borderGap = true;
+        int frames = 20;
+        int phase = 7;
+        for (int i = 0; i < w1; i++) {
+            double d = (period >> 1) * Math.sin((double) i / (double) period + (6.2831853071795862D * phase) / frames);
+            g.copyArea(i, 0, 1, h1, 0, (int) d);
+            if (borderGap) {
+                g.setColor(color);
+                g.drawLine(i, (int) d, i, 0);
+                g.drawLine(i, (int) d + h1, i, h1);
+            }
+        }
+    }
+
+    /**
+     * 将图片转换为二进制数组
+     * @param userImg 图片
+     * @return 图片的二进制数组
+     */
+    public static Byte[] imgToBinary(FileItem userImg) {
+        return null;
     }
 }
