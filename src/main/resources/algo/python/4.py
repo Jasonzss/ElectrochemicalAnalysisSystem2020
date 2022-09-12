68b5c92859aee17bf1d39355d12419b926816995
fzc <1378799690@qq.com>
2022-09-12 21:13:13 +0800

    feat(4.py): 添加了线性回归算法

---
+++ src/main/resources/algo/python/4.py
@@ -0,0 +1,40 @@
+import json
+import sys
+
+import numpy as np
+
+
+def linear_regression(x: np.matrix, y: np.matrix) -> np.matrix: 
+    a = np.linspace(1, 1, y.size)
+    a.resize(y.shape[0], y.shape[1])
+    ans = []
+    if len(y) != 1:
+        x = np.hstack([a, x])
+        ans = (x.T * x).I * x.T * y
+    else:
+        x = np.vstack([a, x])
+        ans = (x * x.T).I * x * y.T
+    return ans
+
+
+if __name__ == "__main__":
+    # arr_data = [[1,90],[2,120],[3,170],[4,210],[5,260]]
+    # data = np.matrix(arr_data) # type: np.matrix
+
+    # 传输数据要注意
+    # 咱们这溶度电流的数据就按这个测试的结构
+    # 那个1， 2， 3， 4就是电流，90，120……就是溶度
+    data = sys.argv[1]
+    data = json.loads(data)
+    data = np.matrix(data) # type: np.matrix
+    # 分割数组，默认最后一列是预测集
+    x, y = np.hsplit(data, [data.shape[1] - 1])
+    ans = linear_regression(x, y)
+    
+    # a0 = round(ans[0, 0], 4)
+    # a1 = round(ans[1, 0], 4)
+    a0 = ans[0, 0]
+    a1 = ans[1, 0]
+    print([a0, a1])
+
+
