2ade8f687efea7b8389d95cf0d4dfc3e1493081d
songjinjin <6020202300@qq.com>
2022-09-19 17:11:30 +0800

    refactor: 修改去均值算法

--- src/main/resources/algo/java/5.java
+++ src/main/resources/algo/java/5.java
@@ -1,19 +1,24 @@
+package algo.java;
+
 /**
  * @Author SDJin
  * @CreationDate 2022/9/3 15:24
- * @Description ：去均值
+ * @Description ：数据预处理去均值
  */
 public class Main {
 
-    public Double[] run(Double[] data) {
-        Double sum = Double.valueOf(0);
-        for (int i = 0; i < data.length; i++) {
-            sum += data[i];
-        }
-        sum /= data.length;
-        for (int i = 0; i < data.length; i++) {
-            data[i] -= sum;
+    public Double[][] run(Double[][] data) {
+        for(int i=0;i<data.length;i++){
+            Double sum = Double.valueOf(0);
+            for (int j = 0; j < data[i].length; j++) {
+                sum += data[i][j];
+            }
+            sum /= data[i].length;
+            for (int j = 0; j < data[i].length; j++) {
+                data[i][j] -= sum;
+            }
         }
         return data;
     }
+
 }
